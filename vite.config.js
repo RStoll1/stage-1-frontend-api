206fbaf5cfe553ae69de571bea9ccf1ad4dd1f45
morti3l <pushthatboard@gmail.com>
2026-01-22 19:23:54 -0600

    Fix errors

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/stage-1-frontend-api/",
   plugins: [react()],
   server: {
     port: 3000,
-    open: true
-  }
+    open: true,
+  },
 })
